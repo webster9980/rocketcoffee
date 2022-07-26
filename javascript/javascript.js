af246984ae3cd641257379d5121b1ce016ebdf15
webster9980 <34650824+webster9980@users.noreply.github.com>
2022-07-26 13:04:35 -0300

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,14 +1,37 @@
-const BtnMobile = document.getElementById('btn-abrir');
+class NavListMobile{
+        constructor(MobileMenu, buttonEvent, Navlink){
+            this.MobileMenu = document.querySelector(MobileMenu)
+            this.buttonEvent = document.querySelector(buttonEvent)
+            this.Navlink = document.querySelectorAll(Navlink)
 
-function NavToggle(){
-    const NavBar = document.getElementById('nav-bar');
-    NavBar.classList.toggle('active');
-}
+            this.HandleClick = this.HandleClick.bind(this);
+        }
+        animateLink(){
+            this.Navlink.forEach((link, index) =>{
+                console.log(index);
+                link.style.animation
+                ?(link.style.animation = "")
+                :(link.style.animation = `navlist 0.5s ease forwards ${index / 7+0.3}s`);
+            });
+        }
+        HandleClick(){
+            this.animateLink();
+            this.MobileMenu.classList.toggle('active')
+        }           
+        addEventListener(){
+            this.buttonEvent.addEventListener("click", this.HandleClick)
+        }
+        init(){
+            if(this.buttonEvent){
+                this.addEventListener();
+            }
+            return this
+        }
+    }
+const MobileMenu = new NavListMobile(
+    ".nav",
+    ".btn-abrir-mobile",
+    ".menu li",
+);
 
-console.log(BtnMobile)
-
-if(BtnMobile != null){
-    BtnMobile.addEventListener('click', NavToggle);
-}else{
-    console.log("ERROR 404");
-}
+MobileMenu.init()
